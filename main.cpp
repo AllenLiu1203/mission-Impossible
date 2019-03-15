b0f7c7e15586350ec2910b4c04ce9c0cbd5ee8c2
AllenLiu1203 <48576577+AllenLiu1203@users.noreply.github.com>
2019-03-15 11:28:17 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,19 @@
+//
+//  main.cpp
+//  homework1
+//
+//  Created by 劉世翔 on 2019/3/15.
+//  Copyright © 2019年 劉世翔. All rights reserved.
+//
+
+#include<stdio.h>
+#include<stdlib.h>
+int main(void)
+{
+    printf("Hello C!\n");
+    printf("Hello World\n");
+    printf("3A713242\n");
+    system("pause");
+    return 0;
+    
+}

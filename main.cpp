6d3032623d1e0e5406334082391f32dad5545626
AllenLiu1203 <48576577+AllenLiu1203@users.noreply.github.com>
2019-03-15 11:36:53 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,19 +1,26 @@
 //
 //  main.cpp
-//  homework1
+//  homework2
 //
 //  Created by 劉世翔 on 2019/3/15.
 //  Copyright © 2019年 劉世翔. All rights reserved.
 //
 
-#include<stdio.h>
-#include<stdlib.h>
-int main(void)
+#include <iostream>
+
+using namespace std;
+
+int main (int argc,char** argv)
 {
-    printf("Hello C!\n");
-    printf("Hello World\n");
-    printf("3A713242\n");
-    system("pause");
+    int key,a,b;
+    cout <<"請輸入你的出生年月日"<<endl;
+    cout<<"年份為";
+    cin>>key;
+    cout<<"月份為";
+    cin>>a;
+    cout<<"日期為";
+    cin>>b;
+    cout<<"輸入出生年月日："<<key<<"年"<<a<<"月"<<b<<"日";
+    system("PAUSE");
     return 0;
-    
 }
